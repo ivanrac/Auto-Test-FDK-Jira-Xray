26635c51dec61f0a192eba92e87fef8e3ec8f9a6
ivanrac <ivo.rac83@gmail.com>
2026-07-06 10:41:42 +0200

    Change login_email to use FDK_LOGIN_USERNAME

--- data/users.js
+++ data/users.js
@@ -2,6 +2,6 @@
 // Obsahuje data definované v Predpoklade FDK-30
 
 export const validUser = {
-  login_email: process.env.FDK_LOGIN_EMAIL,
+  login_email: process.env.FDK_LOGIN_USERNAME,
   login_password: process.env.FDK_LOGIN_PASSWORD
 };
